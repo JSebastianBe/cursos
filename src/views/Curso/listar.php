afd1f1000f55f7a65118357c7d95162b7b25e2d3
JSebastianBe <sebastianbm97@gmail.com>
2022-05-09 15:42:32 -0500

    Tablas dinámicas

--- src/views/Curso/listar.php
+++ src/views/Curso/listar.php
@@ -2,34 +2,36 @@
 <div class="container" id="contenedor">
 <a class="btn boton-s" href="/Cursos/crearCursos" role="button"><i class="bi bi-plus-circle"></i></a>
 <?php require 'src/views/templates/notificaciones.php'; ?>
-<table class="table table-hover">
-	<thead>
-	    <tr>
-			<th scope="col"> idCurso </th>
-			<th scope="col"> Nombre </th>
-			<th scope="col"> Precio </th>
-			<th scope="col"> Duracion </th>
-			<th scope="col"> Profesor </th>
-			<th scope="col"> Detalle </th>
-	    </tr>
-	  </thead>
-	<tbody>
-	<?php 
-	$cursos = $this->d['cursos'];
-	foreach($cursos as $c){
-		?>
-			<tr>
-				<th scope="col"><?php echo $c->getIdCurso(); ?></th>
-				<th><?php echo $c->getNombre(); ?></th>
-				<th><?php echo $c->getPrecio(); ?></th>
-				<th><?php echo $c->getDuracion(); ?></th>
-				<th><?php echo $c->getProfesor(); ?></th>
-		      <th scope="col"><a class="btn boton-p" href="detalleCurso?id=<?php echo $c->getIdCurso(); ?>"><i class="bi bi-zoom-in"></i></a></th>
-		    </tr>
-		<?php
-	}
-	?>
-	 </tbody>
- </table>
+	<div class="table-responsive">
+		<table class="table table-hover">
+			<thead>
+			    <tr>
+					<th scope="col"> idCurso </th>
+					<th scope="col"> Nombre </th>
+					<th scope="col"> Precio </th>
+					<th scope="col"> Duracion </th>
+					<th scope="col"> Profesor </th>
+					<th scope="col"> Detalle </th>
+			    </tr>
+			  </thead>
+			<tbody>
+			<?php 
+			$cursos = $this->d['cursos'];
+			foreach($cursos as $c){
+				?>
+					<tr>
+						<th scope="col"><?php echo $c->getIdCurso(); ?></th>
+						<th><?php echo $c->getNombre(); ?></th>
+						<th><?php echo $c->getPrecio(); ?></th>
+						<th><?php echo $c->getDuracion(); ?></th>
+						<th><?php echo $c->getProfesor(); ?></th>
+				      <th scope="col"><a class="btn boton-p" href="detalleCurso?id=<?php echo $c->getIdCurso(); ?>"><i class="bi bi-zoom-in"></i></a></th>
+				    </tr>
+				<?php
+			}
+			?>
+			 </tbody>
+		 </table>
+	</div>
 </div>
 <?php require 'src/views/templates/footer.php'; ?>
--- src/views/Usuario/listar.php
+++ src/views/Usuario/listar.php
@@ -2,35 +2,40 @@
 <div class="container" id="contenedor">
 <a class="btn boton-s" href="/Cursos/registro" role="button"><i class="bi bi-person-plus"></i></a>
 <?php require 'src/views/templates/notificaciones.php'; ?>
-<table class="table table-hover">
-	<thead>
-	    <tr>
-	      <th scope="col">Nombre</th>
-	      <th scope="col">Telefono</th>
-	      <th scope="col">Correo</th>
-	      <th scope="col">Usuario</th>
-	      <th scope="col">Perfil</th>
-	      <th scope="col">Actualizar</th>
-	    </tr>
-	  </thead>
-	<tbody>
-	<?php 
-	$usuarios = $this->d['usuarios'];
-	foreach($usuarios as $u){
-		?>
-			<tr>
-		      <th><?php echo $u->getNombre(); ?></th>
-		      <th><?php echo $u->getTelefono(); ?></th>
-		      <th><?php echo $u->getCorreo(); ?></th>
-		      <th><?php echo $u->getUsuario(); ?></th>
-		      <th><?php echo $u->getPerfil(); ?></th>
-		      <th scope="col"><a class="btn boton-p" href="modificaUsuario?id=<?php echo $u->getIdUsuario(); ?>"><i class="bi bi-arrow-clockwise"></i></a></th>
+<div class="row">
+	
+</div>
+	<div class="table-responsive">
+		<table class="table table-hover">
+		<thead>
+		    <tr>
+		      <th scope="col">Nombre</th>
+		      <th scope="col">Telefono</th>
+		      <th scope="col">Correo</th>
+		      <th scope="col" >Usuario</th>
+		      <th scope="col">Perfil</th>
+		      <th scope="col">Actualizar</th>
 		    </tr>
-		<?php
-	}
-	?>
-	 </tbody>
- </table>
+		  </thead>
+		<tbody>
+		<?php 
+		$usuarios = $this->d['usuarios'];
+		foreach($usuarios as $u){
+			?>
+				<tr>
+			      <th><?php echo $u->getNombre(); ?></th>
+			      <th><?php echo $u->getTelefono(); ?></th>
+			      <th><?php echo $u->getCorreo(); ?></th>
+			      <th ><?php echo $u->getUsuario(); ?></th>
+			      <th><?php echo $u->getPerfil(); ?></th>
+			      <th scope="col"><a class="btn boton-p" href="modificaUsuario?id=<?php echo $u->getIdUsuario(); ?>"><i class="bi bi-arrow-clockwise"></i></a></th>
+			    </tr>
+			<?php
+		}
+		?>
+		 </tbody>
+	 </table>
+	</div>
 </div>
 <?php require 'src/views/templates/footer.php'; ?>
 
